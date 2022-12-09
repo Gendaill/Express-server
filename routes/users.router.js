9efea3dc03fc62b27c62975849cb1fcab8c36754
James Diaz <James7501@hotmail.com>
2022-12-09 11:29:40 -0500

    Route and service users
    Generated router and service to manage users

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,18 +1,52 @@
 const express = require('express');
 
-const router = express.Router();
+const userService = require('./../services/user.service');
 
-router.get('/', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('No hay parametros');
+const router = express.Router();
+const service = new userService();
+
+router.get('/', async (req, res) => {
+  const users = await service.find();
+  res.json(users);
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await service.findOne(id);
+    res.json(user);
+  } catch (error) {
+    next(error);
   }
 });
+
+router.post('/', async (req, res) => {
+  const body = req.body;
+  const newUser = await service.create(body);
+  res.status(201).json(newUser);
+});
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const user = await service.update(id, body);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const rta = await service.delete(id);
+    res.json(rta);
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 
 module.exports = router;
---
+++ services/user.service.js
@@ -0,0 +1,72 @@
+const { faker } = require('@faker-js/faker');
+const boom = require('@hapi/boom');
+
+class userService {
+
+    constructor() {
+        this.user = [];
+        this.generate();
+    }
+
+    generate() {
+        const limit = 50;
+        for (let index = 0; index < limit; index++) {
+            this.user.push({
+                id: faker.datatype.uuid(),
+                avatar: faker.image.avatar(),
+                birthday: faker.date.birthdate(),
+                email: faker.internet.email(),
+                firstName: faker.name.firstName(),
+                lastName: faker.name.lastName(),
+                sex: faker.name.sexType(),
+            });
+        }
+    }
+
+    async create(data) {
+        const newuser = {
+            id: faker.datatype.uuid(),
+            ...data,
+        }
+        this.user.push(newuser);
+        return newuser;
+    }
+
+    find() {
+        return new Promise((resolve) => {
+            resolve(this.user);
+        })
+    }
+
+    async findOne(id) {
+        const user = this.user.find(item => item.id === id);
+        if (!user) {
+            throw new boom.notFound(`User ${id} not found`);
+        }
+        return user;
+    }
+
+    async  update(id, changes){
+        const index = this.user.findIndex(item => item.id === id);
+        if (index === -1) {
+            throw new boom.notFound(`User ${id} not found`);
+        }
+        const user = this.user[index];
+        this.user[index] = {
+          ...user,
+          ...changes
+        };
+        return this.user[index];
+      }
+
+    async delete(id) {
+        const index = this.user.findIndex(item => item.id === id);
+        if (index === -1) {
+            throw new boom.notFound(`User ${id} not found`);
+        }
+        this.user.splice(index, 1);
+        return { id };
+      }
+}
+
+module.exports = userService;
